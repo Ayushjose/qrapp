8a2da0b7dc706e627a6e1d2da5a2d2ff93b3d9a7
Vaishnavsk7 <160300678+Vaishnavsk7@users.noreply.github.com>
2024-03-18 16:57:19 +0530

    hi
    abc

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:qrapp/qrcode.dart';
 import 'package:qrapp/registration.dart';
 
 class Login extends StatefulWidget {
@@ -12,25 +13,43 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.teal,
+    return Scaffold(backgroundColor: Colors.cyan[200],
       appBar: AppBar(
-        title: Text('Login'),
-        backgroundColor: Colors.lightGreen,
+        title: Text('LOGIN'),
+        backgroundColor: Colors.greenAccent,
       ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(),
-            SizedBox(height: 20),
-            TextField(),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                  hintText: 'Enter Your Username'
+              ),  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter Your  Password'
+              ),  ),
+            ),
             TextButton(
-              onPressed: null,
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>const QrCode()));
+              },
               child: Text('Login'),
               style: ButtonStyle(
                   foregroundColor:
                       MaterialStateProperty.all<Color>(Colors.green)),
+            ),
+            SizedBox(
+              height: 20,
             ),
             RichText(text: TextSpan(text: 'Register here',recognizer: TapGestureRecognizer
               ()..onTap=()=>Navigator.push(context,MaterialPageRoute(builder: (context)=>const Registration()))))
--- lib/registration.dart
+++ lib/registration.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class Registration extends StatefulWidget {
   const Registration({Key? key}) : super(key: key);
 
@@ -10,29 +9,49 @@ class Registration extends StatefulWidget {
 class _RegistrationState extends State<Registration> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.teal,
+    return Scaffold(backgroundColor: Colors.cyan[200],
       appBar: AppBar(
         title: Text('Registration'),
+        backgroundColor: Colors.greenAccent,
       ),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(),
-            SizedBox(
-              height: 20,
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Full Name',
+                  hintText: 'Enter Your Full Name'
+              ),),
             ),
-            TextField(),
-            SizedBox(
-              height: 20,
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'DOB',
+                  hintText: 'Enter Yourc  DOB'
+              ),),
             ),
-            TextField(),
-            SizedBox(
-              height: 20,
+
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                  hintText: 'Enter Your Username'
+              ),),
             ),
-            TextField(),
-            SizedBox(
-              height: 20,
+
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter Your Password'
+              ),),
             ),
+            SizedBox(height: 30,),
             TextButton(onPressed: null, child: Text('Register',style: TextStyle(color: Colors.green),))
           ],
         ),
